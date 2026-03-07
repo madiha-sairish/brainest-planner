197ced1dcea6400b7317d06c6243e80393d11902
Madiha Sairish <f20250297@dubai.bits-pilani.ac.in>
2026-03-07 14:12:17 +0400

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -7,7 +7,7 @@ if(taskText === "") return;
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-tasks.push(taskText);
+tasks.push({text:taskText, done:false});
 
 localStorage.setItem("tasks", JSON.stringify(tasks));
 
@@ -19,7 +19,6 @@ input.value = "";
 function displayTasks(){
 
 let taskList = document.getElementById("taskList");
-
 taskList.innerHTML = "";
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -28,10 +27,29 @@ tasks.forEach(function(task,index){
 
 let li = document.createElement("li");
 
-li.textContent = task;
+let checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+checkbox.checked = task.done;
+
+checkbox.onchange = function(){
+
+tasks[index].done = checkbox.checked;
+
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
+displayTasks();
+
+};
+
+let span = document.createElement("span");
+span.textContent = task.text;
+
+if(task.done){
+span.style.textDecoration = "line-through";
+span.style.color = "gray";
+}
 
 let deleteBtn = document.createElement("button");
-
 deleteBtn.textContent = "Delete";
 
 deleteBtn.onclick = function(){
@@ -44,6 +62,8 @@ displayTasks();
 
 };
 
+li.appendChild(checkbox);
+li.appendChild(span);
 li.appendChild(deleteBtn);
 
 taskList.appendChild(li);
