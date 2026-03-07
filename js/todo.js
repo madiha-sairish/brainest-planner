4844a8ba334a3e713f909b410f6182891331e478
Madiha Sairish <f20250297@dubai.bits-pilani.ac.in>
2026-03-07 13:34:36 +0400

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,26 @@
+function addTask(){
 
+let input = document.getElementById("taskInput");
+
+let taskText = input.value;
+
+if(taskText === "") return;
+
+let li = document.createElement("li");
+
+li.textContent = taskText;
+
+let deleteBtn = document.createElement("button");
+
+deleteBtn.textContent = "Delete";
+
+deleteBtn.onclick = function(){
+li.remove();
+};
+
+li.appendChild(deleteBtn);
+
+document.getElementById("taskList").appendChild(li);
+
+input.value = "";
+}
