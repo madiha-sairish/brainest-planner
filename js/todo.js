7029c6e8fa6d58080bbcaaacd3596a1102ac654e
Madiha Sairish <f20250297@dubai.bits-pilani.ac.in>
2026-03-07 13:46:26 +0400

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,26 +1,55 @@
 function addTask(){
 
 let input = document.getElementById("taskInput");
-
 let taskText = input.value;
 
 if(taskText === "") return;
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+tasks.push(taskText);
+
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
+displayTasks();
+
+input.value = "";
+}
+
+function displayTasks(){
+
+let taskList = document.getElementById("taskList");
+
+taskList.innerHTML = "";
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+tasks.forEach(function(task,index){
+
 let li = document.createElement("li");
 
-li.textContent = taskText;
+li.textContent = task;
 
 let deleteBtn = document.createElement("button");
 
 deleteBtn.textContent = "Delete";
 
 deleteBtn.onclick = function(){
-li.remove();
+
+tasks.splice(index,1);
+
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
+displayTasks();
+
 };
 
 li.appendChild(deleteBtn);
 
-document.getElementById("taskList").appendChild(li);
+taskList.appendChild(li);
 
-input.value = "";
+});
+
 }
+
+displayTasks();
