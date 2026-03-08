e0d344e6be98b764951c0eb2d009941e3b4a0c03
Madiha Sairish <f20250297@dubai.bits-pilani.ac.in>
2026-03-08 13:05:52 +0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,19 @@
+function checkLogin() {
+  let currentUser = localStorage.getItem("currentUser");
+  let currentPage = window.location.pathname.split("/").pop();
 
+  if (!currentUser && currentPage !== "login.html") {
+    window.location.href = "login.html";
+  }
+
+  if (currentUser && currentPage === "login.html") {
+    window.location.href = "index.html";
+  }
+}
+
+function logoutUser() {
+  localStorage.removeItem("currentUser");
+  window.location.href = "login.html";
+}
+
+checkLogin();
