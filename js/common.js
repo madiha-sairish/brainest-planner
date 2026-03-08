b6b788d8da357da8b0c31a417233ef289be32287
Madiha Sairish <f20250297@dubai.bits-pilani.ac.in>
2026-03-08 13:16:07 +0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,23 @@
+function getCurrentUser() {
+  return localStorage.getItem("currentUser");
+}
+
+function getUserKey(keyName) {
+  let currentUser = getCurrentUser();
+  if (!currentUser) return keyName;
+  return currentUser + "_" + keyName;
+}
+
+function saveUserData(keyName, data) {
+  localStorage.setItem(getUserKey(keyName), JSON.stringify(data));
+}
+
+function loadUserData(keyName) {
+  return JSON.parse(localStorage.getItem(getUserKey(keyName))) || [];
+}
+
 function checkLogin() {
-  let currentUser = localStorage.getItem("currentUser");
+  let currentUser = getCurrentUser();
   let currentPage = window.location.pathname.split("/").pop();
 
   if (!currentUser && currentPage !== "login.html") {
@@ -16,4 +34,18 @@ function logoutUser() {
   window.location.href = "login.html";
 }
 
+function applySavedTheme() {
+  let savedTheme = localStorage.getItem("theme") || "light";
+  document.body.classList.remove("light-theme", "dark-theme");
+  document.body.classList.add(savedTheme + "-theme");
+}
+
+function toggleTheme() {
+  let isDark = document.body.classList.contains("dark-theme");
+  let newTheme = isDark ? "light" : "dark";
+  localStorage.setItem("theme", newTheme);
+  applySavedTheme();
+}
+
 checkLogin();
+applySavedTheme();
